98ac6e1e3a124cc866cea154d6176f900de68440
bo0cah <uz_zil@yahoo.co.id>
2017-06-06 14:02:50 +0700

    pgsql

--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
